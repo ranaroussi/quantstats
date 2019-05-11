39bddf9b52aba8b97dfe28c05c186bc7a96ef983
ran <ran@aroussi.com>
2019-05-12 00:58:34 +0300

    removed unused param from to_plotly

--- quantstats/_plotting/wrappers.py
+++ quantstats/_plotting/wrappers.py
@@ -49,9 +49,7 @@ except ImportError:
 
 def to_plotly(fig, title=""):
     if not _HAS_PLOTLY:
-
-        if not show:
-            return fig
+        return fig
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         fig = plotly.tools.mpl_to_plotly(fig)
