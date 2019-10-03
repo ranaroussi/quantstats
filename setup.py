10e1084c51c2fbba48369d606ae74b7d286a9187
ran <ran@aroussi.com>
2019-10-03 18:05:09 +0300

    python 3.5

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
                 quantitative-analysis algo-trading visualization plotting""",
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
     install_requires=['pandas>=0.24.0', 'numpy>=1.15.0', 'scipy>=1.2.0',
-                      'matplotlib==3.1.0', 'seaborn>=0.9.0',
+                      'matplotlib>=3.0.0', 'seaborn>=0.9.0',
                       'tabulate>=0.8.0', 'yfinance>=0.1.44'],
     entry_points={
         'console_scripts': [
