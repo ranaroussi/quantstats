f7d28b84109b50506f2fc03cd83a2f3eaee2a5a5
ran <ran@aroussi.com>
2021-03-24 10:39:31 +0000

    Set next release as "Production/Stable"

--- setup.py
+++ setup.py
@@ -44,8 +44,8 @@ setup(
         # 'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
-        'Development Status :: 4 - Beta',
-        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         'Operating System :: OS Independent',
 
