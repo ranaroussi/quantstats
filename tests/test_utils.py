95a45ccec820e28278851558ffaaac9af6a19332
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-04-26 15:34:21 +0400

    testing, testing, testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,2 +1,6 @@
-def test_make_index(portfolio):
-    assert True
+from quantstats.utils import to_excess_returns
+
+
+def test_excess_returns(returns):
+    r = to_excess_returns(returns, rf=0.01)
+    print(r)
